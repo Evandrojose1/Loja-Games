de8f14f5bfc8085c6b8c1f9675a253fb558c4e38
Evandro <evandrojosepereirasilva@gmail.com>
2023-09-21 09:46:18 -0300

    Loja-Games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'inicio', component: InicioComponent},
+  {path:'login', component: LoginComponent}, 
+ {path:'', redirectTo:'/inicio',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
